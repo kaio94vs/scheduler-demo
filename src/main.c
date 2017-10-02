e4d7bb7ca3ec0732973882514d3c22ff44fea97f
Kaio Vieira Dos Santos - 156072 <ra156072@le20-9.grad.fee.unicamp.br>
2017-10-02 17:46:10 -0300

    Finalizado

--- src/main.c
+++ src/main.c
@@ -1,12 +1,34 @@
+/*Kaio Vieira dos Santos
+ RA:156072*/
+
 #include <stdio.h>
 
 int main() {
   int a[5];
-  int n;
+  int n, j,m;
+  int flag=0;
 
   scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
 
-  for (int i=0; i<n; i++) {
+  int i;
+  for (i=1; i<=n; i++) {
+    flag=0;
+    for(j=0;j<5; j++) {
+      
+      
+      
+      if  ( i%a[j] ==0 ){
+	printf ("%d", j);
+	flag=1;
+      }
+    }
+    
+      if (flag==0)
+	printf ("-");
+      
+      printf ("\n");
+     
+    
     /* Imprime resultado da n-esima iteracao do scheduler */
   }
 
